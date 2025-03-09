ed5f98157e813b45a88fc93c9f3f817d3825cb09
Romulo Sobreira <sobreiraromuloes@hotmail.com>
2025-03-09 07:23:36 -0300

    feat: add new job with chunks and new propertie

---
+++ src/main/java/br/com/sobreiraromulo/primeiroprojeot/ParImparBatchConfig.java
@@ -0,0 +1,69 @@
+package br.com.sobreiraromulo.primeiroprojeot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.function.FunctionItemProcessor;
+import org.springframework.batch.item.support.IteratorItemReader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class ParImparBatchConfig {
+
+    private JobRepository jobRepository;
+    private PlatformTransactionManager transactionManager;
+
+    public ParImparBatchConfig(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        this.jobRepository = jobRepository;
+        this.transactionManager = transactionManager;
+    }
+
+    @Bean
+    public Job impreParImparJob() {
+        return new JobBuilder("impreParImparJob", jobRepository)
+                .start(imprimeParImparStep())
+                .incrementer(new RunIdIncrementer())
+                .build();
+    }
+
+    @Bean
+    public Step imprimeParImparStep() {
+        return new StepBuilder("imprimeParImparSetp", jobRepository)
+                .<Integer, String>chunk(1, transactionManager)
+                .reader(contaAteDezReader())
+                .processor(parOuImparProcessor())
+                .writer(imprimeWriter())
+                .build();
+
+    }
+
+    @StepScope
+    @Bean
+    public IteratorItemReader<Integer> contaAteDezReader() {
+        List<Integer> numerosDeUmAteDez = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        return new IteratorItemReader<Integer>(numerosDeUmAteDez.iterator());
+    }
+
+    public FunctionItemProcessor<Integer, String> parOuImparProcessor() {
+        return new FunctionItemProcessor<Integer, String>(item -> item % 2 == 0 ? String.format("Item %s é Par", item)
+                : String.format("Item  %s é impar", item));
+    }
+
+    public ItemWriter<String> imprimeWriter() {
+        return items -> items.forEach(System.out::println);
+    }
+
+}
